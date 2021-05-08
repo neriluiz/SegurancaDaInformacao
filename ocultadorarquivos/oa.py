5af54e29644a0989e886494d77996b7f5e50bf4e
Neri Luiz von Holleben <neri_luiz@yahoo.com.br>
2021-05-08 14:54:54 -0400

    Com input do usuario

--- ocultadorarquivos/oa.py
+++ ocultadorarquivos/oa.py
@@ -2,6 +2,8 @@
 
 import ctypes
 
+pasta = input("Digite o caminho da pasta a ser ocultada ex (C:/pasta): ")
+
 atributo_ocultar = 0x02
 
 retorno = ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt', atributo_ocultar)
